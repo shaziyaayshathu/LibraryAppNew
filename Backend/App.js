6af4fe258f15e97c1fd77db0b421e5c22495c6b1
shaziyaayshathu <shaziyaayshathu@gmail.com>
2022-11-26 00:18:12 +0530

    change

---
+++ Backend/App.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const cors=require('cors');
+const logger=require('morgan');//for seeing api calls in the terminal
+const bodyParser=require('body-parser')
+
+
+const app=new express();
+
+require('./Middlewares/mongoDb') // to initialise mongoose
+
+const PORT=3200
+app.use(bodyParser.json())
+app.use(cors())          //to connect frontend and backend without any disturbances
+app.use(express.json())       //to receive data from front end
+app.use(express.urlencoded({extended:true}))
+app.use(logger('dev'))
+
+
+const api=require('./routes/api')
+app.use('/api',api)
+
+app.listen(PORT,()=>{
+    console.log(`.......Server is listening at port ${PORT}........`)
+
+})
